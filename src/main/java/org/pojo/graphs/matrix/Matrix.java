3795c4d0b074caf08d81e5f97aeb89b5a8202277
Piotr Joński <yoyo@wp.eu>
2016-03-10 20:25:39 +0100

    Mnożenie macierzy przez macierz

--- src/main/java/org/pojo/graphs/matrix/Matrix.java
+++ src/main/java/org/pojo/graphs/matrix/Matrix.java
@@ -5,46 +5,46 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 public class Matrix {
 
-    private final int n;
-    private final int m;
+    private final int rows;
+    private final int columns;
 
     private final long[][] matrix;
 
-    public Matrix(final int n) {
-        this(n, n);
+    public Matrix(final int squareSize) {
+        this(squareSize, squareSize);
     }
 
-    public Matrix(final int n, final int m) {
-        matrix = new long[n][m];
-        this.n = n;
-        this.m = m;
+    public Matrix(final int rows, final int columns) {
+        matrix = new long[rows][columns];
+        this.rows = rows;
+        this.columns = columns;
     }
 
-    public void setValue(final long value, final int n, final int m) {
-        if (n > this.n || m > this.m) {
+    public Matrix(final long[][] matrix) {
+        this.matrix = matrix;
+        this.rows = matrix.length;
+        this.columns = matrix[0].length;
+    }
+
+    public void setValue(final long value, final int row, final int column) {
+        if (row > this.rows || column > this.columns) {
             throw new UnsupportedOperationException("Nie można ustawić wartości dla pola spoza zakresu macierzy!");
         }
-        final int row = translateToJavaNumbering(n);
-        final int column = translateToJavaNumbering(m);
-
         matrix[row][column] = value;
     }
 
-    public long getValue(final int n, final int m) {
-        if (n > this.n || m > this.m) {
+    public long getValue(final int row, final int column) {
+        if (row > this.rows || column > this.columns) {
             throw new UnsupportedOperationException("Nie można odczytać wartości dla pola spoza zakresu macierzy!");
         }
-        final int row = translateToJavaNumbering(n);
-        final int column = translateToJavaNumbering(m);
-
         return matrix[row][column];
     }
 
     public Matrix add(final Matrix matrix) {
         checkAddOrSubPreconditions(matrix);
-        final Matrix sumOfMatrix = new Matrix(n, m);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        final Matrix sumOfMatrix = new Matrix(rows, columns);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
                 final long newValue = getValue(i, j) + matrix.getValue(i, j);
                 sumOfMatrix.setValue(newValue, i, j);
             }
@@ -54,9 +54,9 @@ public class Matrix {
 
     public Matrix sub(final Matrix matrix) {
         checkAddOrSubPreconditions(matrix);
-        final Matrix sumOfMatrix = new Matrix(n, m);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        final Matrix sumOfMatrix = new Matrix(rows, columns);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
                 final long newValue = getValue(i, j) - matrix.getValue(i, j);
                 sumOfMatrix.setValue(newValue, i, j);
             }
@@ -65,11 +65,26 @@ public class Matrix {
     }
 
     public Matrix multiply(final long multiplyNumber) {
-        final Matrix multipliedMatrix = new Matrix(n, m);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        final Matrix multipliedMatrix = new Matrix(rows, columns);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
                 final long newValue = getValue(i, j) * multiplyNumber;
                 multipliedMatrix.setValue(newValue, i, j);
+            }
+        }
+        return multipliedMatrix;
+    }
+
+    public Matrix multiply(final Matrix otherMatrix) {
+        checkMultiplyPreconditions(otherMatrix);
+        final Matrix multipliedMatrix = new Matrix(rows, otherMatrix.columns);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < otherMatrix.columns; j++) {
+                long temp = 0;
+                for (int w = 0; w < otherMatrix.rows; w++) {
+                    temp += getValue(i, w) * otherMatrix.getValue(w, j);
+                }
+                multipliedMatrix.setValue(temp, i, j);
             }
         }
         return multipliedMatrix;
@@ -78,14 +93,14 @@ public class Matrix {
     @Override
     public String toString() {
         final StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < rows; i++) {
             stringBuilder.append("|\t");
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j < columns; j++) {
                 stringBuilder.append(matrix[i][j])
                              .append("\t");
             }
             stringBuilder.append("|");
-            if (i + 1 < n) {
+            if (i + 1 < rows) {
                 stringBuilder.append("\n");
             }
         }
@@ -93,11 +108,11 @@ public class Matrix {
     }
 
     public int getRows() {
-        return n;
+        return rows;
     }
 
     public int getColumns() {
-        return m;
+        return columns;
     }
 
     @Override
@@ -112,24 +127,18 @@ public class Matrix {
 
         final Matrix matrix1 = (Matrix) o;
 
-        return new EqualsBuilder().append(n, matrix1.n)
-                                  .append(m, matrix1.m)
+        return new EqualsBuilder().append(rows, matrix1.rows)
+                                  .append(columns, matrix1.columns)
                                   .append(matrix, matrix1.matrix)
                                   .isEquals();
     }
 
     @Override
     public int hashCode() {
-        return new HashCodeBuilder().append(n)
-                                    .append(m)
+        return new HashCodeBuilder().append(rows)
+                                    .append(columns)
                                     .append(matrix)
                                     .toHashCode();
-    }
-
-    private int translateToJavaNumbering(final int n) {
-        return n == 0
-               ? 0
-               : n - 1;
     }
 
     private void checkMultiplyPreconditions(final Matrix matrix) {
--- src/test/java/org/pojo/graphs/matrix/MatrixTest.java
+++ src/test/java/org/pojo/graphs/matrix/MatrixTest.java
@@ -1,8 +1,6 @@
 package org.pojo.graphs.matrix;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.pojo.test.TestHelper;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,15 +9,32 @@ import static org.powermock.reflect.Whitebox.getInternalState;
 public class MatrixTest {
 
     @Test
+    public void shouldCreateMatrixFormTwoDimensionalArray() {
+        // given
+        final long[][] matrix = {{5, 1}, {4, 2}};
+        final Matrix expectedResult = new Matrix(2, 2);
+        expectedResult.setValue(5, 0, 0);
+        expectedResult.setValue(1, 0, 1);
+        expectedResult.setValue(4, 1, 0);
+        expectedResult.setValue(2, 1, 1);
+
+        // when
+        final Matrix result = new Matrix(matrix);
+
+        // then
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
     public void shouldPrintFormattedSquareMatrixAfterValuesSet() {
         // given
         final Matrix matrix = new Matrix(2);
         final String expectedOutput = "|\t1\t2\t|\n" +
                                       "|\t3\t4\t|";
-        matrix.setValue(1, 1, 1);
-        matrix.setValue(2, 1, 2);
-        matrix.setValue(3, 2, 1);
-        matrix.setValue(4, 2, 2);
+        matrix.setValue(1, 0, 0);
+        matrix.setValue(2, 0, 1);
+        matrix.setValue(3, 1, 0);
+        matrix.setValue(4, 1, 1);
 
         // when
         final String result = matrix.toString();
@@ -61,8 +76,8 @@ public class MatrixTest {
         final long expectedResult = 1L;
 
         // when
-        matrix.setValue(expectedResult, 2, 2);
-        final long result = getInternalStateOfMatrix(matrix, 2, 2);
+        matrix.setValue(expectedResult, 1, 1);
+        final long result = getInternalStateOfMatrix(matrix, 1, 1);
 
         // then
         assertThat(result).isEqualTo(expectedResult);
@@ -73,10 +88,10 @@ public class MatrixTest {
         // given
         final Matrix matrix = new Matrix(2, 2);
         final long expectedResult = 1L;
-        setInternalStateOfMatrix(matrix, expectedResult, 2, 2);
+        setInternalStateOfMatrix(matrix, expectedResult, 1, 1);
 
         // when
-        final long result = matrix.getValue(2, 2);
+        final long result = matrix.getValue(1, 1);
 
         // then
         assertThat(result).isEqualTo(expectedResult);
@@ -111,6 +126,45 @@ public class MatrixTest {
     }
 
     @Test
+    public void shouldMultiplyByOtherMatrix1() {
+        // given
+        final long[][] matrix1Array = {{2, 1, 3}, {-1, 2, 4}};
+        final Matrix matrix1 = new Matrix(matrix1Array);
+        final long[][] matrix2Array = {{1, 3}, {2, -2}, {-1, 4}};
+        final Matrix matrix2 = new Matrix(matrix2Array);
+        final long[][] expectedMatrixArray = {{1, 16}, {-1, 9}};
+        final Matrix expectedMatrix = new Matrix(expectedMatrixArray);
+
+        // when
+        final Matrix result = matrix1.multiply(matrix2);
+
+        // then
+        assertThat(result).isEqualTo(expectedMatrix);
+    }
+
+    @Test
+    public void shouldMultiplyByOtherMatrix2() {
+        // given
+        final long[][] matrix1Array = {{5, 1}, {-3, 3}, {4, -1}, {-1, 2}, {-0, 7}};
+        final Matrix matrix1 = new Matrix(matrix1Array);
+        final long[][] matrix2Array = {{1, 7, 0, -1, 9, 0, 5}, {2, -6, -1, 10, 2, -3, 1}};
+        final Matrix matrix2 = new Matrix(matrix2Array);
+        final long[][] expectedMatrixArray = {{7, 29, -1, 5, 47, -3, 26},
+                                              {3, -39, -3, 33, -21, -9, -12},
+                                              {2, 34, 1, -14, 34, 3, 19},
+                                              {3, -19, -2, 21, -5, -6, -3},
+                                              {14, -42, -7, 70, 14, -21, 7}};
+        final Matrix expectedMatrix = new Matrix(expectedMatrixArray);
+
+        // when
+        final Matrix result = matrix1.multiply(matrix2);
+
+        // then
+        assertThat(result).isEqualTo(expectedMatrix);
+    }
+
+
+    @Test
     public void shouldSubtractTwoMatrix() {
         // given
         final Matrix matrix1 = createAndFillWith(1, 2, 3);
@@ -136,12 +190,12 @@ public class MatrixTest {
 
     private long getInternalStateOfMatrix(final Matrix matrix, final int n, final int m) {
         final long[][] targetMatrix = getInternalState(matrix, "matrix");
-        return targetMatrix[n - 1][m - 1];
+        return targetMatrix[n][m];
     }
 
     private void setInternalStateOfMatrix(final Matrix matrix, final long value, final int n, final int m) throws Exception {
         final long[][] targetMatrix = getInternalState(matrix, "matrix");
-        targetMatrix[n - 1][m - 1] = value;
+        targetMatrix[n][m] = value;
         TestHelper.setFinalStatic(matrix, "matrix", targetMatrix);
     }
 
